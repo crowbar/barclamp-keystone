01690aba6dff62fcc488e03ee685dcbd18443eb6
Justin Shepherd <jshepher@rackspace.com>
2011-11-03 12:53:24 -0500

    completed server recipe

--- chef/cookbooks/keystone/recipes/server.rb
+++ chef/cookbooks/keystone/recipes/server.rb
@@ -1,81 +1,141 @@
-user = node[:openstack][:keystone][:user]
-uid = node[:openstack][:keystone][:uid]
-grp = node[:openstack][:keystone][:group]
+# Copyright 2011 Dell, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-user node[:openstack][:keystone][:user] do
-   uid uid
-   group grp
-   home "/home/keystone"
+package "keystone" do
+  action :install
 end
 
-package "keystone"
-
-%w{python-pastedeploy python-eventlet python-routes python-sqlalchemy}.each { |pkg|
-  package pkg
-}
-
-def python_truth(v)
-  v ? "True" : "False"
+execute "Fix Bug lp:865448" do
+  command "sed -i 's/path.abspath(sys.argv\[0\])/path.dirname(__file__)/g' /usr/share/pyshared/keystone/controllers/version.py"
+  action :run
 end
 
-def nodes_addresses(nodes, network)
-  result = []
-  nodes.each { |n|
-    result << node_addr(n, network)
-  }
-  result
+service "keystone" do
+  supports :status => true, :restart => true
+  action :enable
 end
 
-def node_addr(n, network)
-  ip = Chef::Recipe::Barclamp::Inventory.get_network_by_type(n, network).address
-  ip ||= Chef::Recipe::Barclamp::Inventory.get_network_by_type(n, "admin").address
-  ip
+node.set_unless['keystone']['db']['password'] = secure_password
+
+if node[:keystone][:sql_engine] == "mysql"
+    Chef::Log.info("Configuring Keystone to use MySQL backend")
+
+    include_recipe "mysql::client"
+
+    package "python-mysqldb" do
+        action :install
+    end
+
+    mysqls = search(:node, "recipes:mysql\\:\\:server") || []
+    if mysqls.length > 0
+        mysql = mysqls[0]
+    else
+        mysql = node
+    end
+
+    mysql_address = Chef::Recipe::Barclamp::Inventory.get_network_by_type(mysql, "admin").address if mysql_address.nil?
+    Chef::Log.info("Mysql server found at #{mysql_address}")
+    
+    # Create the Keystone Database
+    mysql_database "create #{node[:keystone][:db][:database]} database" do
+        host    mysql_address
+        username "db_maker"
+        password mysql[:mysql][:db_maker_password]
+        database node[:keystone][:db][:database]
+        action :create_db
+    end
+
+    mysql_database "create dashboard database user" do
+        host    mysql_address
+        username "db_maker"
+        password mysql[:mysql][:db_maker_password]
+        database node[:keystone][:db][:database]
+        action :query
+        sql "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER on #{node[:keystone][:db][:database]}.* to '#{node[:keystone][:db][:user]}'@'%' IDENTIFIED BY '#{node[:keystone][:db][:password]}';"
+    end
+    sql_connection = "mysql://#{node[:keystone][:db][:user]}:#{node[:keystone][:db][:password]}@#{mysql_address}/#{node[:keystone][:db][:database]}"
+elsif node[:keystone][:sql_engine] == "sqlite"
+    Chef::Log.info("Configuring Keystone to use SQLite backend")
+    sql_connection = "sqlite:////var/lib/keystone/keystone.db"
 end
 
-debug = python_truth(node[:openstack][:keystone][:debug])
+my_ipaddress = Chef::Recipe::Barclamp::Inventory.get_network_by_type(node, "admin").address
+
+debug = True
+verbose = True
 
 template "/etc/keystone/keystone.conf" do
     source "keystone.conf.erb"
     mode "0644"
     owner user
     group grp
-    variables ( { :debug => debug, :verbose => debug })
+    variables (
+      :sql_connection => sql_connection,
+      :debug => debug,
+      :verbose => verbose
+    )
+    notifies :restart, resources(:services => "keystone"), :immediately
 end
 
-cfg_name = node[:keystone][:config][:environment].gsub("keystone-config-","")
-nova_api_filter = "recipes:nova\\:\\:api AND nova_config_environment:nova-config-#{cfg_name}"
-swift_api_filter = "recipes:swift\\:\\:proxy AND swift_config_environment:swift-config-#{cfg_name}"
-glance_api_filter = "recipes:glance\\:\\:api AND glance_config_environment:glance-config-#{cfg_name}"
-
-nova_nodes = search(:node,nova_api_filter)
-swift_nodes = search(:node,swift_api_filter)
-glance_nodes = search(:node,glance_api_filter)
-
-nova_addrs = nodes_addresses(nova_nodes, "public")
-swift_addrs = nodes_addresses(swift_nodes, "public")
-glance_addrs = nodes_addresses(glance_nodes, "admin")
-
-Chef::Log.debug("Keystone: glance_addrs = #{glance_addrs.nil? ? "NIL" : glance_addrs.inspect}")
-Chef::Log.debug("Keystone: swift_addrs = #{swift_addrs.nil? ? "NIL" : swift_addrs.inspect}")
-Chef::Log.debug("Keystone: nova_addrs = #{nova_addrs.nil? ? "NIL" : nova_addrs.inspect}")
-
-keystone_ip = node_addr(node, "public")
-
-execute "keystone initial data" do
-  command "/tmp/initial_data.sh"
-  action :nothing
+# Create default tenant
+execute "Keystone: add <default> tenant" do
+  command "keystone-manage tenant add #{node[:keystone][:default][:tenant]}"
+  action :run
+  not_if "keystone-manage tenant list|grep #{node[:keystone][:default][:tenant]}"
 end
 
-template "/tmp/initial_data.sh" do
-  source "initial_data.sh.erb"
-  mode "0755"
-  owner user
-  group grp
-  variables ( { :nova_addrs => nova_addrs, :swift_addrs => swift_addrs, :glance_addrs => glance_addrs, :keystone_addr => keystone_ip })
-  notifies :run, "execute[keystone initial data]", :immediately
+# Create admin user
+execute "Keystone: add <admin> user" do
+  command "keystone-manage user add #{node[:keystone][:admin][:username]} #{node[:keystone][:admin][:password]} #{node[:keystone][:default][:tenant]}"
+  action :run
+  not_if "keystone-manage user list|grep #{node[:keystone][:admin][:username]}"
 end
 
-service "keystone" do
-  action [:enable, :start ]
+# Create admin token
+execute "Keystone: add <admin> user token" do
+  command "keystone-manage token add #{node[:keystone][:admin][:token]} #{node[:keystone][:admin][:username]} #{node[:keystone][:default][:tenant]} #{node[:keystone][:admin][:'token-expiration']"
+  action :run
+  not_if "keystone-manage token list | grep #{node[:keystone][:admin][:token]}"
 end
 
+# Create Admin role
+execute "Keystone: add ServiceAdmin role" do
+  command "keystone-manage role add Admin"
+  action :run
+  not_if "keystone-manage role list|grep Admin"
+end
+
+# Create Member role
+execute "Keystone: add Member role" do
+  command "keystone-manage role add Member"
+  action :run
+  not_if "keystone-manage role list|grep Member"
+end
+
+execute "Keystone: grant ServiceAdmin role to <admin> user" do
+  # command syntax: role grant 'role' 'user' 'tenant (optional)'
+  command "keystone-manage role grant Admin #{node[:keystone][:admin][:username]}"
+  action :run
+end
+
+execute "Keystone: grant Admin role to <admin> user for <default> tenant" do
+  # command syntax: role grant 'role' 'user' 'tenant (optional)'
+  command "keystone-manage role grant Admin #{node[:keystone][:admin][:username]} #{node[:keystone][:default][:tenant]}"
+  action :run
+  not_if "keystone-manage role list #{node[:keystone][:default][:tenant]}|grep Admin"
+end
+
+node[:keystone][:monitor][:svcs] <<["keystone-server"]
+node.save
