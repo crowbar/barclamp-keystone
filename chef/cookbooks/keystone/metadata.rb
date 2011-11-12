2f180e3d9f9f7d99a4b6b3e4a40e9d3f10479ec0
Greg Althaus <galthaus@austin.rr.com>
2011-11-12 08:42:45 -0600

    Make sure we depend upon openssl so that when we use sqlite we have access to passwords

--- chef/cookbooks/keystone/metadata.rb
+++ chef/cookbooks/keystone/metadata.rb
@@ -5,4 +5,4 @@ description      "Openstack Keystone server deployment recipes."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "1.0"
 
-depends "apt"
+depends "openssl"
