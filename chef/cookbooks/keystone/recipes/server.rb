5a0c893caf9de6e7910b455c06d35b412a4a6174
Vincent Untz <vuntz@suse.com>
2013-05-31 17:09:04 +0200

    Port to database barclamp
    A good part of this was made by Ralf Haferkamp <rhafer@suse.de>

--- chef/cookbooks/keystone/recipes/server.rb
+++ chef/cookbooks/keystone/recipes/server.rb
@@ -121,60 +121,79 @@ elsif node[:keystone][:frontend]=='apache'
   end
 end
 
+database_engine = node[:keystone][:database_engine]
 
+Chef::Log.info("Configuring Keystone to use #{database_engine} backend")
 
-::Chef::Recipe.send(:include, Opscode::OpenSSL::Password)
+if database_engine == "database"
 
-node.set_unless['keystone']['db']['password'] = secure_password
-
-if node[:keystone][:sql_engine] == "mysql"
-    Chef::Log.info("Configuring Keystone to use MySQL backend")
-
-    include_recipe "mysql::client"
-
-    package "python-mysqldb" do
-        package_name "python-mysql" if node.platform == "suse"
-        action :install
-    end
-
-    env_filter = " AND mysql_config_environment:mysql-config-#{node[:keystone][:mysql_instance]}"
-    mysqls = search(:node, "roles:mysql-server#{env_filter}") || []
-    if mysqls.length > 0
-        mysql = mysqls[0]
-        mysql = node if mysql.name == node.name
+    env_filter = " AND database_config_environment:database-config-#{node[:keystone][:database_instance]}"
+    sqls = search(:node, "roles:database-server#{env_filter}") || []
+    if sqls.length > 0
+        sql = sqls[0]
+        sql = node if sql.name == node.name
     else
-        mysql = node
+        sql = node
     end
+    include_recipe "database::client"
+    backend_name = Chef::Recipe::Database::Util.get_backend_name(sql)
+    include_recipe "#{backend_name}::client"
+    include_recipe "#{backend_name}::python-client"
 
-    mysql_address = Chef::Recipe::Barclamp::Inventory.get_network_by_type(mysql, "admin").address if mysql_address.nil?
-    Chef::Log.info("Mysql server found at #{mysql_address}")
-    
+    db_provider = Chef::Recipe::Database::Util.get_database_provider(sql)
+    db_user_provider = Chef::Recipe::Database::Util.get_user_provider(sql)
+    privs = Chef::Recipe::Database::Util.get_default_priviledges(sql)
+    url_scheme = backend_name
+
+    ::Chef::Recipe.send(:include, Opscode::OpenSSL::Password)
+    node.set_unless['keystone']['db']['password'] = secure_password
+
+
+    sql_address = Chef::Recipe::Barclamp::Inventory.get_network_by_type(sql, "admin").address if sql_address.nil?
+    Chef::Log.info("Database server found at #{sql_address}")
+
+    db_conn = { :host => sql_address,
+                :username => "db_maker",
+                :password => sql[database_engine][:db_maker_password] }
+
     # Create the Keystone Database
-    mysql_database "create #{node[:keystone][:db][:database]} database" do
-        host    mysql_address
-        username "db_maker"
-        password mysql[:mysql][:db_maker_password]
-        database node[:keystone][:db][:database]
-        action :create_db
+    database "create #{node[:keystone][:db][:database]} database" do
+        connection db_conn
+        database_name node[:keystone][:db][:database]
+        provider db_provider
+        action :create
     end
 
-    mysql_database "create dashboard database user" do
-        host    mysql_address
-        username "db_maker"
-        password mysql[:mysql][:db_maker_password]
-        database node[:keystone][:db][:database]
-        action :query
-        sql "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER on #{node[:keystone][:db][:database]}.* to '#{node[:keystone][:db][:user]}'@'%' IDENTIFIED BY '#{node[:keystone][:db][:password]}';"
+    database_user "create keystone database user" do
+        connection db_conn
+        username node[:keystone][:db][:user]
+        password node[:keystone][:db][:password]
+        host '%'
+        provider db_user_provider
+        action :create
     end
-    sql_connection = "mysql://#{node[:keystone][:db][:user]}:#{node[:keystone][:db][:password]}@#{mysql_address}/#{node[:keystone][:db][:database]}"
-elsif node[:keystone][:sql_engine] == "sqlite"
-    Chef::Log.info("Configuring Keystone to use SQLite backend")
+
+    database_user "grant database access for keystone database user" do
+        connection db_conn
+        username node[:keystone][:db][:user]
+        password node[:keystone][:db][:password]
+        database_name node[:keystone][:db][:database]
+        host '%'
+        privileges privs
+        provider db_user_provider
+        action :grant
+    end
+    sql_connection = "#{url_scheme}://#{node[:keystone][:db][:user]}:#{node[:keystone][:db][:password]}@#{sql_address}/#{node[:keystone][:db][:database]}"
+elsif database_engine == "sqlite"
     sql_connection = "sqlite:////var/lib/keystone/keystone.db"
     file "/var/lib/keystone/keystone.db" do
         owner node[:keystone][:user]
         action :create_if_missing
     end
+else
+    Chef::Log.error("Unknown database engine #{database_engine}")
 end
+
 
 template "/etc/keystone/keystone.conf" do
     source "keystone.conf.erb"
