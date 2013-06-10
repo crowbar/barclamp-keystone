e6fa8a1ea3919c18d8ac0303d5aecafe6a9b342e
ArkadyKanevsky <arkady_kanevsky@dell.com>
2013-06-10 13:06:31 -0500

    enforcing single proposal for keystone

--- crowbar_framework/app/models/keystone_service.rb
+++ crowbar_framework/app/models/keystone_service.rb
@@ -19,9 +19,9 @@ class KeystoneService < ServiceObject
     @bc_name = "keystone"
     @logger = thelogger
   end
-
+# Turn off multi proposal support till it really works and people ask for it.
   def self.allow_multiple_proposals?
-    true
+    false
   end
 
   def proposal_dependencies(role)
