e917dda6b02f77eefd057a06d7f49ecbe2e8e565
Vincent Untz <vuntz@suse.com>
2013-12-24 09:41:40 +0100

    Make 001_ldap_user_enabled_default_str_to_bool.rb more solid
    We can really convert the value, so let's do it.

--- chef/data_bags/crowbar/migrate/keystone/001_ldap_user_enabled_default_str_to_bool.rb
+++ chef/data_bags/crowbar/migrate/keystone/001_ldap_user_enabled_default_str_to_bool.rb
@@ -1,9 +1,9 @@
 def upgrade ta, td, a, d
-  a['ldap']['user_enabled_default'] = true
+  a['ldap']['user_enabled_default'] = (a['ldap']['user_enabled_default'] == 'true')
   return a, d
 end
 
 def downgrade ta, td, a, d
-  a['ldap']['user_enabled_default'] = 'true'
+  a['ldap']['user_enabled_default'] = a['ldap']['user_enabled_default'] ? 'true' : 'false'
   return a, d
 end
