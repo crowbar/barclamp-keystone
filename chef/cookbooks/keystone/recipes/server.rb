4e34bc31a403ef35b2f35727e6fa93926c668bdf
Evgeniy Shurmin <eshurmin@mirantis.com>
2014-02-07 12:30:45 +0400

    remove temporary "unless" block for RHEL family

--- chef/cookbooks/keystone/recipes/server.rb
+++ chef/cookbooks/keystone/recipes/server.rb
@@ -412,26 +412,24 @@ user_roles.each do |args|
 end
 
 # Create EC2 creds for our users
-unless platform?("redhat", "centos", "fedora")
-  ec2_creds = [
-    [node[:keystone][:admin][:username], node[:keystone][:admin][:tenant]],
-    [node[:keystone][:admin][:username], node[:keystone][:default][:tenant]],
-    [node[:keystone][:default][:username], node[:keystone][:default][:tenant]]
-  ]
-  ec2_creds.each do |args|
-    keystone_register "add default ec2 creds for #{args[1]}:#{args[0]}" do
-      protocol node[:keystone][:api][:protocol]
-      host my_admin_host
-      auth ({
-          :tenant => node[:keystone][:admin][:tenant],
-          :user => node[:keystone][:admin][:username],
-          :password => node[:keystone][:admin][:password]
-      })
-      port node[:keystone][:api][:admin_port]
-      user_name args[0]
-      tenant_name args[1]
-      action :add_ec2
-    end
+ec2_creds = [
+  [node[:keystone][:admin][:username], node[:keystone][:admin][:tenant]],
+  [node[:keystone][:admin][:username], node[:keystone][:default][:tenant]],
+  [node[:keystone][:default][:username], node[:keystone][:default][:tenant]]
+]
+ec2_creds.each do |args|
+  keystone_register "add default ec2 creds for #{args[1]}:#{args[0]}" do
+    protocol node[:keystone][:api][:protocol]
+    host my_admin_host
+    auth ({
+        :tenant => node[:keystone][:admin][:tenant],
+        :user => node[:keystone][:admin][:username],
+        :password => node[:keystone][:admin][:password]
+    })
+    port node[:keystone][:api][:admin_port]
+    user_name args[0]
+    tenant_name args[1]
+    action :add_ec2
   end
 end
 
