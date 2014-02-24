dcf83417e9f625b742c39bfdfbedb334ff28c53a
Chris Dearborn <Christopher_Dearborn@dell.com>
2014-02-24 18:08:39 -0500

    Added Berkshelf support.

--- crowbar_engine/barclamp_keystone/app/models/barclamp_keystone/server.rb
+++ crowbar_engine/barclamp_keystone/app/models/barclamp_keystone/server.rb
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-class BarclampKeystone::Server < Role
+class BarclampKeystone::Server < BarclampChef::Role
   include BarclampOpenstack
 
 # Event triggers for node creation and destruction.
