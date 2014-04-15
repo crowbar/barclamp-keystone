262c371bbe1a36658ba01e850520afb7f58cdcaa
Vincent Untz <vuntz@suse.com>
2014-04-15 09:51:34 +0200

    Do the db sync only on initial run, and on all platforms

--- chef/cookbooks/keystone/recipes/server.rb
+++ chef/cookbooks/keystone/recipes/server.rb
@@ -328,10 +328,9 @@ execute "keystone-manage db_sync" do
   user node[:keystone][:user]
   group node[:keystone][:group]
   action :run
-  # On SUSE, we only need this when HA is enabled as the init script is doing
-  # this (but that creates races with HA); we only care about it for the
-  # initial sync, though, so we'll do that once, on the founder.
-  only_if { node.platform != "suse" || (!node[:keystone][:db_synced] && ha_enabled && CrowbarPacemakerHelper.is_cluster_founder?(node)) }
+  # We only do the sync the first time, and only if we're not doing HA or if we
+  # are the founder of the HA cluster (so that it's really only done once).
+  only_if { !node[:keystone][:db_synced] && (!ha_enabled || CrowbarPacemakerHelper.is_cluster_founder?(node)) }
 end
 
 # We want to keep a note that we've done db_sync, so we don't do it again.
