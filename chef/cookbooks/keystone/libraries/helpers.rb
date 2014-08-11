3a6d86d81d46ffef64c636522b3a8c6750bba462
Vincent Untz <vuntz@suse.com>
2014-08-11 14:01:48 +0200

    Cache keystone node per instance when fetching keystone settings
    Since keystone settings depend on each cookbook (due to the service
    user/password), we can't cache the settings per keystone instance.
    However, we can avoid searching for the keystone node all the time by
    caching the node per instance.

--- chef/cookbooks/keystone/libraries/helpers.rb
+++ chef/cookbooks/keystone/libraries/helpers.rb
@@ -51,14 +51,25 @@ module KeystoneHelper
 
   private
   def self.search_for_keystone(node, cookbook_name)
-    nodes, _, _ = Chef::Search::Query.new.search(:node, "roles:keystone-server AND keystone_config_environment:keystone-config-#{node[cookbook_name][:keystone_instance]}")
+    instance = node[cookbook_name][:keystone_instance]
+
+    if @keystone_node && @keystone_node.include?(instance)
+      Chef::Log.info("Keystone server found at #{@keystone_node[instance][:keystone][:api][:internal_URL_host]} [cached]")
+      return @keystone_node[instance]
+    end
+
+    nodes, _, _ = Chef::Search::Query.new.search(:node, "roles:keystone-server AND keystone_config_environment:keystone-config-#{instance}")
     if nodes.first
       keystone_node = nodes.first
       keystone_node = node if keystone_node.name == node.name
     else
       keystone_node = node
     end
-    Chef::Log.info("Keystone server found at #{keystone_node[:keystone][:api][:internal_URL_host]}")
-    return keystone_node
+
+    @keystone_node ||= Hash.new
+    @keystone_node[instance] = keystone_node
+
+    Chef::Log.info("Keystone server found at #{@keystone_node[instance][:keystone][:api][:internal_URL_host]}")
+    return @keystone_node[instance]
   end
 end
