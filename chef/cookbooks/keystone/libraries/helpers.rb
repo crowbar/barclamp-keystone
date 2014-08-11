b273cfe8768b6537dfc07d27619c47f0ee708f7b
Vincent Untz <vuntz@suse.com>
2014-08-11 14:08:45 +0200

    Reorganize KeystoneHelper.keystone_settings

--- chef/cookbooks/keystone/libraries/helpers.rb
+++ chef/cookbooks/keystone/libraries/helpers.rb
@@ -9,43 +9,42 @@ module KeystoneHelper
 
   def self.keystone_settings(current_node, cookbook_name)
     # cache the result for each cookbook in an instance variable hash
-    if @keystone_settings && @keystone_settings.include?(cookbook_name)
-      return @keystone_settings[cookbook_name]
+    unless @keystone_settings && @keystone_settings.include?(cookbook_name)
+      node = search_for_keystone(current_node, cookbook_name)
+
+      use_ssl = node["keystone"]["api"]["protocol"] == "https"
+      if node[:keystone][:api][:versioned_public_URL].nil? || node[:keystone][:api][:public_URL_host].nil?
+        # only compute this if we don't have the right attributes yet; this will
+        # be fixed on next run of chef-client on keystone node
+        public_host = CrowbarHelper.get_host_for_public_url(node, use_ssl)
+      end
+      @keystone_settings ||= Hash.new
+      @keystone_settings[cookbook_name] = {
+        "admin_auth_url" => node[:keystone][:api][:admin_URL] || service_URL(node, node[:fqdn], node["keystone"]["api"]["admin_port"]),
+        "public_auth_url" => node[:keystone][:api][:versioned_public_URL] || versioned_service_URL(node, public_host, node["keystone"]["api"]["service_port"]),
+        "internal_auth_url" => node[:keystone][:api][:versioned_internal_URL] || versioned_service_URL(node, node[:fqdn], node["keystone"]["api"]["service_port"]),
+        "use_ssl" => use_ssl,
+        "endpoint_region" => node["keystone"]["api"]["region"],
+        "insecure" => use_ssl && node[:keystone][:ssl][:insecure],
+        "protocol" => node["keystone"]["api"]["protocol"],
+        "public_url_host" => node[:keystone][:api][:public_URL_host] || public_host,
+        "internal_url_host" => node[:keystone][:api][:internal_URL_host] || node[:fqdn],
+        "service_port" => node["keystone"]["api"]["service_port"],
+        "admin_port" => node["keystone"]["api"]["admin_port"],
+        "admin_token" => node["keystone"]["service"]["token"],
+        "admin_tenant" => node["keystone"]["admin"]["tenant"],
+        "admin_user" => node["keystone"]["admin"]["username"],
+        "admin_password" => node["keystone"]["admin"]["password"],
+        "default_tenant" => node["keystone"]["default"]["tenant"],
+        "default_user" => node["keystone"]["default"]["username"],
+        "default_password" => node["keystone"]["default"]["password"],
+        "service_tenant" => node["keystone"]["service"]["tenant"]
+      }
+
+      @keystone_settings[cookbook_name]['service_user'] = current_node[cookbook_name][:service_user]
+      @keystone_settings[cookbook_name]['service_password'] = current_node[cookbook_name][:service_password]
     end
 
-    node = search_for_keystone(current_node, cookbook_name)
-
-    use_ssl = node["keystone"]["api"]["protocol"] == "https"
-    if node[:keystone][:api][:versioned_public_URL].nil? || node[:keystone][:api][:public_URL_host].nil?
-      # only compute this if we don't have the right attributes yet; this will
-      # be fixed on next run of chef-client on keystone node
-      public_host = CrowbarHelper.get_host_for_public_url(node, use_ssl)
-    end
-    @keystone_settings ||= Hash.new
-    @keystone_settings[cookbook_name] = {
-      "admin_auth_url" => node[:keystone][:api][:admin_URL] || service_URL(node, node[:fqdn], node["keystone"]["api"]["admin_port"]),
-      "public_auth_url" => node[:keystone][:api][:versioned_public_URL] || versioned_service_URL(node, public_host, node["keystone"]["api"]["service_port"]),
-      "internal_auth_url" => node[:keystone][:api][:versioned_internal_URL] || versioned_service_URL(node, node[:fqdn], node["keystone"]["api"]["service_port"]),
-      "use_ssl" => use_ssl,
-      "endpoint_region" => node["keystone"]["api"]["region"],
-      "insecure" => use_ssl && node[:keystone][:ssl][:insecure],
-      "protocol" => node["keystone"]["api"]["protocol"],
-      "public_url_host" => node[:keystone][:api][:public_URL_host] || public_host,
-      "internal_url_host" => node[:keystone][:api][:internal_URL_host] || node[:fqdn],
-      "service_port" => node["keystone"]["api"]["service_port"],
-      "admin_port" => node["keystone"]["api"]["admin_port"],
-      "admin_token" => node["keystone"]["service"]["token"],
-      "admin_tenant" => node["keystone"]["admin"]["tenant"],
-      "admin_user" => node["keystone"]["admin"]["username"],
-      "admin_password" => node["keystone"]["admin"]["password"],
-      "default_tenant" => node["keystone"]["default"]["tenant"],
-      "default_user" => node["keystone"]["default"]["username"],
-      "default_password" => node["keystone"]["default"]["password"],
-      "service_tenant" => node["keystone"]["service"]["tenant"]
-    }
-
-    @keystone_settings[cookbook_name]['service_user'] = current_node[cookbook_name][:service_user]
-    @keystone_settings[cookbook_name]['service_password'] = current_node[cookbook_name][:service_password]
     @keystone_settings[cookbook_name]
   end
 
