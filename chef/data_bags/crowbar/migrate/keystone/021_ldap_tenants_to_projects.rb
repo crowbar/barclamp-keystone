76451a8eee6c02e85133eae23254cb8fdf58c5d5
Ionuț Arțăriși <iartarisi@suse.cz>
2014-11-20 11:35:31 +0100

    rename ldap tenant_* attributes to project_*

---
+++ chef/data_bags/crowbar/migrate/keystone/021_ldap_tenants_to_projects.rb
@@ -0,0 +1,17 @@
+def upgrade ta, td, a, d
+  %w{ tree_dn filter objectclass domain_id_attribute id_attribute member_attribute name_attribute desc_attribute enabled_attribute attribute_ignore allow_create allow_update allow_delete enabled_emulation enabled_emulation_dn }.each do |attr|
+    a['ldap']["project_#{attr}"] = a['ldap']["tenant_#{attr}"]
+    a['ldap'].delete "tenant_#{attr}"
+  end
+
+  return a, d
+end
+
+def downgrade ta, td, a, d
+  %w{ tree_dn filter objectclass domain_id_attribute id_attribute member_attribute name_attribute desc_attribute enabled_attribute attribute_ignore allow_create allow_update allow_delete enabled_emulation enabled_emulation_dn }.each do |attr|
+    a['ldap']["tenant_#{attr}"] = a['ldap']["project_#{attr}"]
+    a['ldap'].delete "project_#{attr}"
+  end
+
+  return a, d
+end
