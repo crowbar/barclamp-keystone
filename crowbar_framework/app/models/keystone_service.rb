96a6e5b3a97cc78077cddaf89261ee0f185278b8
Vincent Untz <vuntz@suse.com>
2015-05-11 18:40:12 +0200

    Allow deploying keystone on SLE12 nodes
    This seems to just work fine.

--- crowbar_framework/app/models/keystone_service.rb
+++ crowbar_framework/app/models/keystone_service.rb
@@ -33,7 +33,6 @@ class KeystoneService < PacemakerServiceObject
           "unique" => false,
           "count" => 1,
           "exclude_platform" => {
-            "suse" => "12.0",
             "windows" => "/.*/"
           },
           "cluster" => true
